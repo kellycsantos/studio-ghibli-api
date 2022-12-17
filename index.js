6bc64d2aaddf82e355d0594652bafc4e42564eaf
Kelly Santos <kellycristinasntos@gmail.com>
2022-12-16 22:00:57 -0300

    Ajusted cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const moviesData = require("./data.json")
 const PORT = process.env.PORT || 9000;
 
+const cors = require("cors")
+
+app.use(cors({methods: ["GET"]}))
 
 app.get("/", (req,res) => {
     res.json(
